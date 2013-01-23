71e2f2857e38bae793cbe13f9ad5722b026eb294
david <david@rd.netease.com>
2013-01-23 19:38:52 +0800

    MOD separate win implementation ADD ansi supporting

--- ct_test.go
+++ ct_test.go
@@ -8,7 +8,7 @@ import(
 func TestChangeColor(t *testing.T) {
     defer ResetColor()
     fmt.Println("Normal text...")
-    text := "This is an demo of using ChangeColor to output colorful texts."
+    text := "This is an demo of using ChangeColor to output colorful texts"
     i := 1
     for _, c := range(text) {
         ChangeColor(Color(i / 2 % 8) + Black, i % 2 == 1, Color((i + 2) / 2 % 8) + Black, false)
@@ -18,6 +18,9 @@ func TestChangeColor(t *testing.T) {
     fmt.Println()
     ChangeColor(Red, true, White, false)
     fmt.Println("Before reset.")
+    ChangeColor(Red, false, White, true)
+    fmt.Println("Before reset.")
     ResetColor()
+    fmt.Println("After reset.")
     fmt.Println("After reset.")
 }
